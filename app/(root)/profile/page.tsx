8acc5f58a7ac64c7a97244b2d471c43dfe14865b
unknown <me_badis@yahoo.fr>
2024-02-04 20:11:42 +0100

    Adding profile route and fixing cardLink

---
+++ app/(root)/profile/page.tsx
@@ -0,0 +1,72 @@
+import Page from "@/app/(auth)/sign-in/[[...sign-in]]/page"
+import Collection from "@/components/shared/Collection"
+import { Button } from "@/components/ui/button"
+import { getEventsByUser } from "@/lib/actions/event.actions"
+import { auth } from "@clerk/nextjs"
+import Link from "next/link"
+
+const ProfilePage = async () => {
+    const {sessionClaims} = auth()
+    const userId = sessionClaims?.userId as string
+
+    const oranizedEvents = await getEventsByUser({userId, page: 1})
+  return (
+    <>
+    {/* My Tickets*/}
+        <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+            <div className="wrapper flex items-center justify-center sm:justify-between">
+                <h3 className="h3-bold text-center sm:text-left">
+                    My Tickets
+                </h3>
+                <Button asChild size="lg" className="button hidden sm:flex">
+                    <Link href="/#events">
+                        Explore More Events
+                    </Link>
+                </Button>
+            </div>
+        </section>
+
+        {/* <section className="wrapper my-8">
+        <Collection
+          data={relatedEvents?.data}
+          emptyTitle="No event tickets purchased yet"
+          emptyStateSubtext='No worries - plenty of exciting events to explore!'
+          collectiontype="My_Tickets"
+          limit={3}
+          page={1}
+          urlParamName="ordersPage"
+          totalPages={2}
+        />
+        </section> */}
+    {/* Events Organizd*/}
+        <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+                <div className="wrapper flex items-center justify-center sm:justify-between">
+                    <h3 className="h3-bold text-center sm:text-left">
+                        Events Organized
+                    </h3>
+                    <Button asChild size="lg" className="button hidden sm:flex">
+                        <Link href="/events/create">
+                            Create New Event
+                        </Link>
+
+                    </Button>
+                </div>
+        </section>
+
+        <section className="wrapper my-8">
+            <Collection
+            data={oranizedEvents?.data}
+            emptyTitle="No events have been created yet"
+            emptyStateSubtext='Go create an event and start selling tickets!'
+            collectiontype="Events_Organized"
+            limit={6}
+            page={1}
+            urlParamName="eventsPage"
+            totalPages={2}
+            />
+        </section>
+    </>
+  )
+}
+
+export default ProfilePage
--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -43,8 +43,7 @@ const Card = ({event, hasOrderLink, hidePrice}: CardProps) => {
                 </div>
             )
         }
-        <Link
-            href={`/events/${event._id}`}
+        <div
             className="flex min-h-[200px] flex-col gap-3 p-5 md:gap-4"
         >
             {!hidePrice && 
@@ -60,10 +59,11 @@ const Card = ({event, hasOrderLink, hidePrice}: CardProps) => {
             <p className="p-medium-16 p-medium-18 text-grey-500">
                 {formatDateTime(event.startDateTime).dateTime}
             </p>
-               
-            <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">
-                {event.title}
-            </p>
+            <Link href={`/events/${event._id}`}>   
+                <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">
+                    {event.title}
+                </p>
+            </Link>
 
             <div className="flex-between w-full">
                 <p className="p-medium-14 md:p-medium-16 text-grey-600">
@@ -81,7 +81,7 @@ const Card = ({event, hasOrderLink, hidePrice}: CardProps) => {
                     </Link>
                 )}
             </div>
-        </Link>
+        </div>
     </div>
   )
 }
